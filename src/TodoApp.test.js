25ef59520841057f044dd5b01acc53e8abc9d164
Steven Dowd <swdowd@gmail.com>
2023-09-29 14:15:07 -0400

    Continued testing, primarily for TodoApp

---
+++ src/TodoApp.test.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { render, fireEvent, debug } from "@testing-library/react";
+import TodoApp from "./TodoApp";
+
+describe("TodoApp component", function() {
+  const initialTodos=[
+    {
+      id: 1,
+      title: "Code!",
+      description: "Write some code",
+      priority: 2,
+    },
+    {
+      id: 2,
+      title: "Make dinner",
+      description: "Cook something healthy",
+      priority: 1,
+    },
+    {
+      id: 3,
+      title: "Go to bed",
+      description: "In bed by 11:15",
+      priority: 3,
+    },
+  ]
+
+  test("renders without crashing", function(){
+    render(<TodoApp initialTodos={initialTodos}/>)
+  });
+
+  test("renders new todo upon form submission", function(){
+    //const { container, debug } = render(<TodoApp initialTodos={initialTodos}/>);
+
+    const result = render(<TodoApp initialTodos={initialTodos}/>);
+    const titleField = document.getElementById("newTodo-title");
+    const descriptionField = document.getElementById("newTodo-description");
+    const priorityField = document.getElementById("newTodo-priority");
+    const todoForm = document.querySelector(".NewTodoForm");
+
+    fireEvent.change(titleField, {target: {value: "newTitle"}});
+    fireEvent.change(descriptionField, {target: {value: "newDesc"}});
+    fireEvent.change(priorityField, {target: {value: 1}});
+
+    fireEvent.submit(todoForm, {target: {title: {value: "newTitle"},
+                                        description: {value: "newDesc"},
+                                       priority: {value: 1}}});
+
+    const allTodos = document.querySelectorAll(".Todo");
+
+    expect(result.queryByText("newTitle")).toBeInTheDocument();
+    //Four listed, one in the top.
+    expect(allTodos.length).toBe(5);
+  });
+
+  test("rendering when initialTodos is empty", function(){
+    const result = render(<TodoApp initialTodos={[]}/>);
+
+    const allTodos = document.querySelectorAll(".Todo");
+
+    expect(result.queryByText("You have no todos.")).toBeInTheDocument();
+    expect(allTodos.length).toBe(0);
+
+  });
+
+  test("deletion of a todo", function() {
+    const result = render(<TodoApp initialTodos={[{
+      id: 2,
+      title: "Make dinner",
+      description: "Cook something healthy",
+      priority: 1,
+    }]}/>);
+
+    const firstDelButton = document.querySelector(".EditableTodo-delBtn");
+    //const firstTodo = document.querySelector(".Todo");
+
+    fireEvent.click(firstDelButton);
+
+    const allTodos = document.querySelectorAll(".Todo");
+
+    expect(allTodos.length).toBe(0);
+    expect(result.queryByText("Make dinner")).not.toBeInTheDocument();
+  });
+});
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("Todo form component", function (){
@@ -18,16 +18,38 @@ describe("Todo form component", function (){
       handleSave={() => console.log("test handle save")}
       />);
 
-    expect(result.queryByText("Code!")).toBeInTheDocument();
-    expect(result.queryByText("Make dinner")).toBeInTheDocument();
-    expect(result.queryByText("Go to bed")).toBeInTheDocument();
+    const titleField = document.getElementById("newTodo-title");
+    const descriptionField = document.getElementById("newTodo-description");
+    const priorityField = document.getElementById("newTodo-priority");
+
+    expect(titleField).toBeInTheDocument();
+    expect(descriptionField).toBeInTheDocument();
+    expect(priorityField).toBeInTheDocument();
+  });
+
+  test("changes state with changes to form inputs", function(){
+    const result = render(<TodoForm
+      initialFormData={BLANK_FORM_DATA}
+      handleSave={() => console.log("test handle save")}
+      />);
+
+    const titleField = document.getElementById("newTodo-title");
+    const descriptionField = document.getElementById("newTodo-description");
+    const priorityField = document.getElementById("newTodo-priority");
+
+    fireEvent.change(titleField, {target: {value: "newTitle"}});
+    fireEvent.change(descriptionField, {target: {value: "newDesc"}});
+    fireEvent.change(priorityField, {target: {value: 1}});
+
+    expect(titleField.value).toBe("newTitle");
+    expect(descriptionField.value).toBe("newDesc");
+    expect(priorityField.value).toBe("1");
   });
 
   test("passes snapshot", function (){
-    const { container } = render(<EditableTodoList
-      todos={initialTodos}
-      update={() => console.log("test update")}
-      remove={() => console.log("test remove")}
+    const { container } = render(<TodoForm
+      initialFormData={BLANK_FORM_DATA}
+      handleSave={() => console.log("test handle save")}
       />);
     expect(container).toMatchSnapshot();
   });
